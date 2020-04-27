cc7424fa57e1b0d46d4414a568aafc376d237120
零起飞 <lx_howlet@163.com>
2020-04-27 15:49:33 +0800

    静态文件

---
+++ public/static/module/admin/js/lib/init.js
@@ -0,0 +1,410 @@
+
+//自定义js
+
+//公共配置
+
+
+$(document).ready(function () {
+    //菜单点击
+    //J_iframe
+
+    //$(document).pjax('a.J_menuItem', '.J_mainContent');
+    //点击左侧菜单栏目
+    $(".J_menuItem").on('click',function(){
+        var url = $(this).attr('href');
+        $("#J_iframe").attr('src',url);
+        return false;
+    });
+
+    $("[data-toggle='tooltip']").tooltip();
+
+    // MetsiMenu
+    $('#side-menu').metisMenu();
+
+    // 打开右侧边栏
+    $('.right-sidebar-toggle').click(function () {
+        $('#right-sidebar').toggleClass('sidebar-open');
+    });
+
+    //固定菜单栏
+    $(function () {
+        $('.sidebar-collapse').slimScroll({
+            height: '100%',
+            railOpacity: 0.9,
+            alwaysVisible: false
+        });
+    });
+
+
+    // 菜单切换
+    $('.navbar-minimalize').click(function () {
+        $("body").toggleClass("mini-navbar");
+        SmoothlyMenu();
+    });
+
+
+    // 侧边栏高度
+    function fix_height() {
+        var heightWithoutNavbar = $("body > #wrapper").height() - 61;
+        $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
+    }
+
+    fix_height();
+
+    $(window).bind("load resize click scroll", function () {
+        if (!$("body").hasClass('body-small')) {
+            fix_height();
+        }
+    });
+
+    //侧边栏滚动
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav')) {
+            $('#right-sidebar').addClass('sidebar-top');
+        } else {
+            $('#right-sidebar').removeClass('sidebar-top');
+        }
+    });
+
+    $('.full-height-scroll').slimScroll({
+        height: '100%'
+    });
+
+    $('#side-menu>li').click(function () {
+        if ($('body').hasClass('mini-navbar')) {
+            NavToggle();
+        }
+    });
+    $('#side-menu>li li a').click(function () {
+        if ($(window).width() < 769) {
+            NavToggle();
+        }
+    });
+
+    $('.nav-close').click(NavToggle);
+
+    //ios浏览器兼容性处理
+    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+        $('#content-main').css('overflow-y', 'auto');
+    }
+
+    // //日期选择插件yyyy-mm-dd
+    $(".datepicker").datepicker({
+        language: "zh-CN",
+        minView : 'month',
+        todayHighlight:true,
+        autoclose: true,//选中之后自动隐藏日期选择框
+        clearBtn: true,//清除按钮
+        todayBtn: "linked",//今日按钮
+        format: "yyyy-mm-dd"
+    });
+
+    // //日期时间选择插件 yyyy-mm-dd H:i:s
+    $(".datetimepicker").datetimepicker({
+        language: "zh-CN",
+        autoclose: true,//选中之后自动隐藏日期选择框
+        clearBtn: true,//清除按钮
+        todayBtn: true,//今日按钮
+        format: "yyyy-mm-dd hh:ii:ss",
+    });
+
+    // //日期时间选择插件 yyyy-mm-dd H:i:s
+    $(".datetimepicker-clock").datetimepicker({
+        language: "zh-CN",
+        startView:'day',
+        //minView : 'day',
+        //maxView:2,
+        autoclose: true,//选中之后自动隐藏日期选择框
+        clearBtn: true,//清除按钮
+        todayBtn: true,//今日按钮
+        format: "hh:ii",
+    });
+
+    // //日期时间选择插件 yyyy-mm-dd H:i:s
+    $(".datetimepicker-now").datetimepicker({
+        language: "zh-CN",
+        autoclose: true,//选中之后自动隐藏日期选择框
+        clearBtn: true,//清除按钮
+        todayBtn: true,//今日按钮
+        format: "yyyy-mm-dd hh:ii:ss",
+        initialDate:new Date(),
+    });
+
+
+    // //日期时间选择插件 yyyy-mm-dd H:i:s
+    $('.clockpicker').clockpicker();
+
+    //选择框效果
+    $('.i-checks').iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        radioClass: 'iradio_square-green',
+    });
+
+    //bootstrap 下拉选择
+    $('.chosen-select').chosen({search_contains: true});
+
+
+});
+
+$(window).bind("load resize", function () {
+    if ($(this).width() < 769) {
+        $('body').addClass('mini-navbar');
+        // $('.navbar-static-side').fadeIn();
+    }
+});
+
+function NavToggle() {
+    $('.navbar-minimalize').trigger('click');
+}
+
+function SmoothlyMenu() {
+    if (!$('body').hasClass('mini-navbar')) {
+        $('#side-menu').hide();
+        setTimeout(
+            function () {
+                $('#side-menu').fadeIn(500);
+            }, 100);
+    } else if ($('body').hasClass('fixed-sidebar')) {
+        $('#side-menu').hide();
+        setTimeout(
+            function () {
+                $('#side-menu').fadeIn(500);
+            }, 300);
+    } else {
+        $('#side-menu').removeAttr('style');
+    }
+}
+
+/**
+  * 将form里面的内容序列化成json
+  * 相同的checkbox用分号拼接起来
+  * @param {dom} 指定的选择器
+  * @param {obj} 需要拼接在后面的json对象
+  * @method serializeJson
+  * */
+$.fn.serializeJson = function (otherString) {
+    var serializeObj = {},
+        array = this.serializeArray();
+    $(array).each(function () {
+        if (serializeObj[this.name]) {
+            serializeObj[this.name] += ';' + this.value;
+        } else {
+            serializeObj[this.name] = this.value;
+        }
+    });
+
+    if (otherString != undefined) {
+        var otherArray = otherString.split(';');
+        $(otherArray).each(function () {
+            var otherSplitArray = this.split(':');
+            serializeObj[otherSplitArray[0]] = otherSplitArray[1];
+        });
+    }
+    return serializeObj;
+};
+
+/**
+ * 将josn对象赋值给form
+ * @param {dom} 指定的选择器
+ * @param {obj} 需要给form赋值的json对象
+ * @method serializeJson
+ * */
+$.fn.setForm = function (jsonValue) {
+    var obj = this;
+    $.each(jsonValue, function (name, ival) {
+        var $oinput = obj.find("input[name=" + name + "]");
+        if ($oinput.attr("type") == "checkbox") {
+            if (ival !== null) {
+                var checkboxObj = $("[name=" + name + "]");
+                var checkArray = ival.split(";");
+                for (var i = 0; i < checkboxObj.length; i++) {
+                    for (var j = 0; j < checkArray.length; j++) {
+                        if (checkboxObj[i].value == checkArray[j]) {
+                            checkboxObj[i].click();
+                        }
+                    }
+                }
+            }
+        } else if ($oinput.attr("type") == "radio") {
+            $oinput.each(function () {
+                var radioObj = $("[name=" + name + "]");
+                for (var i = 0; i < radioObj.length; i++) {
+                    if (radioObj[i].value == ival) {
+                        radioObj[i].click();
+                    }
+                }
+            });
+        } else if ($oinput.attr("type") == "textarea") {
+            obj.find("[name=" + name + "]").html(ival);
+        } else {
+            obj.find("[name=" + name + "]").val(ival);
+        }
+    })
+}
+
+/**
+ * 操纵toastor的便捷类
+ * @type {{success: success, error: error, info: info, warning: warning}}
+ */
+var toast = {
+    /**
+     * 成功提示
+     * @param text 内容
+     * @param title 标题
+     */
+    success: function (text, title) {
+
+        $(".toast").remove();
+
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "positionClass": "toast-top-center",
+            "onclick": null,
+            "showDuration": "1000",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+        toastr.success(text, title);
+    },
+    /**
+     * 失败提示
+     * @param text 内容
+     * @param title 标题
+     */
+    error: function (text, title) {
+
+        $(".toast").remove();
+
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "positionClass": "toast-top-center",
+            "onclick": null,
+            "showDuration": "1000",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+        toastr.error(text, title);
+    },
+    /**
+     * 信息提示
+     * @param text 内容
+     * @param title 标题
+     */
+    info: function (text, title) {
+
+        $(".toast").remove();
+
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "positionClass": "toast-top-center",
+            "onclick": null,
+            "showDuration": "1000",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+        toastr.info(text, title);
+    },
+    /**
+     * 警告提示
+     * @param text 内容
+     * @param title 标题
+     */
+    warning: function (text, title) {
+
+        $(".toast").remove();
+
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "positionClass": "toast-top-center",
+            "onclick": null,
+            "showDuration": "1000",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+        toastr.warning(text, title);
+    }
+};
+
+/**
+ * 搜索表单url
+ */
+var searchFormUrl = function (obj) {
+
+    var url = $(obj).attr('url');
+    var query = $('.search-form').find('input,select').serialize();
+    query = query.replace(/(&|^)(\w*?\d*?\-*?_*?)*?=?((?=&)|(?=$))/g, '');
+    query = query.replace(/^&/g, '');
+    if (url.indexOf('?') > 0) {
+        url += '&' + query;
+    } else {
+        url += '?' + query;
+    }
+
+    return url;
+};
+
+
+//将url转化为json数据
+function url2json(url){
+    let arr=[]; //存储参数的数组
+    let res={}; //存储最终JSON结果对象
+    arr=url.split("?")[1].split("&"); //arr=["a=1", "b=2", "c=test", "d"]
+
+    for(let i=0,len=arr.length;i<len;i++){
+        //如果有等号，则执行赋值操作
+        if(arr[i].indexOf("=")!=-1){
+            let str=arr[i].split("=");
+            //str=[a,1];
+            res[str[0]]=str[1];
+        }else{//没有等号，则赋予空值
+            res[arr[i]]="";
+        }
+    }
+    res=JSON.stringify(res);//转化为JSON字符串
+    return res; //{"a": "1", "b": "2", "c": "test", "d": ""}
+}
+//增加必填选项
+//$('input[required]').before('<span style="color:red">*</span>');
+//$('input[required]').parents("div.form-group").addClass('has-error');
+//$('input[required]').tooltip({title:'必填写',  placement:'right'});
+$("input[type='text']").attr('autocomplete','off');
+
+
+
+// /*-----页面pannel内容区高度自适应 start-----*/
+// $(window).resize(function () {
+//     setCenterHeight();
+// });
+// setCenterHeight();
+// function setCenterHeight() {
+//     var height = $(window).height();
+//     var centerHight = height - 70;
+//     $(".float-e-margins").height(centerHight).css("overflow", "auto");
+// }
+/*-----页面pannel内容区高度自适应 end-----*/
+
+
