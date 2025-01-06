c09769262425e1b9cad63d213d4b3e1cd8d27008
goodkfrs <goodkfrs@qq.com>
2025-01-06 10:35:38 +0800

    1，修复图片地址不兼容问题

--- app/common.php
+++ app/common.php
@@ -670,27 +670,18 @@ function get_picture_url($id = 0, $member = 'picture')
 {
 
     $fileLogic = get_sington_object('fileLogic', LogicFile::class);
-
     if (is_numeric($id)) {
         return $fileLogic->getPictureUrl($id, $member);
     } else {
         //1、表示为多张图的id 如：1，2，3，4
         if (strpos($id, '/') === false) {
-
             return $fileLogic->getPictureUrl($id, $member);
-
         } else if (strpos($id, 'http') === 0) {//表示外网路路不要转换 如：http://www.07fly.com/upload/img/demo.jpg
-
             return $id;
-
         } else if (strpos($id, '/') === 0) {//表示路径为绝对路路不要转换 如：/upload/img/demo.jpg
-
             return $id;
-
         } else {//表示为相对路径：imgaes/demo.jpg
-
             return $fileLogic->getPictureWebUrl($id);
-
         }
     }
 }
